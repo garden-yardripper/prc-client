19284a7b301f237c21a63c8b9483378c1b64bc5c
garden-yardripper <gardenyardripper@gmail.com>
2026-06-08 22:17:38 -0400

    Add synchronous server getting methods to Client

--- src/prc/v2/client.py
+++ src/prc/v2/client.py
@@ -92,4 +92,29 @@ class AsyncClient(_GetServer):
         return await self._get_bundled_server_async()
 
 class Client(_GetServer):
-    pass
+    def get_server(
+        self, *,
+        players: bool = False,
+        staff: bool = False,
+        join_logs: bool = False,
+        queue: bool = False,
+        kill_logs: bool = False,
+        command_logs: bool = False,
+        mod_calls: bool = False,
+        emergency_calls: bool = False,
+        vehicles: bool = False
+    ) -> Server:
+        return self._get_server_sync(
+            players=players,
+            staff=staff,
+            join_logs=join_logs,
+            queue=queue,
+            kill_logs=kill_logs,
+            command_logs=command_logs,
+            mod_calls=mod_calls,
+            emergency_calls=emergency_calls,
+            vehicles=vehicles
+        )
+    
+    def get_bundled_server(self) -> BundledServer:
+        return self._get_bundled_server_sync()
