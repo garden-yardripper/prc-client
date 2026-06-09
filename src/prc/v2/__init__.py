1c57ebe5160ac66997fce6c7bf13b6cc8f92952b
gardenyardripper-ai <gardenyardripper@gmail.com>
2026-06-09 10:12:10 -0400

    Remove unnecessary model imports from v2 __init__

--- src/prc/v2/__init__.py
+++ src/prc/v2/__init__.py
@@ -1,29 +1,3 @@
 from .client import Client, AsyncClient
-from .models import (
-    UsernameUser,
-    IdUser,
-    FullUser,
-    MinimalLocation,
-    Location,
-    Vehicle,
-    EmergencyCall,
-    JoinLog,
-    KillLog,
-    CommandLog,
-    ModCall,
-    Queue,
-    Staff,
-    Player,
-    Server,
-    BundledServer
-)
 
-__all__ = [
-    "Client", "AsyncClient",
-    "UsernameUser", "IdUser", "FullUser",
-    "MinimalLocation", "Location",
-    "Vehicle", "EmergencyCall",
-    "JoinLog", "KillLog", "CommandLog", "ModCall",
-    "Queue", "Staff", "Player",
-    "Server", "BundledServer"
-]
+__all__ = ["Client", "AsyncClient"]
