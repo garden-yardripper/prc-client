713a59b5fc810746a62da0819747763ef0f07777
garden-yardripper <gardenyardripper@gmail.com>
2026-06-11 17:44:57 -0400

    Fix username reference in examples

--- examples/server/get_server.py
+++ examples/server/get_server.py
@@ -4,7 +4,7 @@ def main():
     with prc.v2.Client(server_key="...") as client:
         server = client.get_bundled_server()
         for player in server.players:
-            print(f"{player.user.username} is on the {player.team} team!")
+            print(f"{player.user.name} is on the {player.team} team!")
     
 if __name__ == "__main__":
     main()
--- examples/server/get_server_async.py
+++ examples/server/get_server_async.py
@@ -5,7 +5,7 @@ async def main():
     async with prc.v2.AsyncClient(server_key="...") as client:
         server = await client.get_bundled_server()
         for player in server.players:
-            print(f"{player.user.username} is on the {player.team} team!")
+            print(f"{player.user.name} is on the {player.team} team!")
     
 if __name__ == "__main__":
     asyncio.run(main())
