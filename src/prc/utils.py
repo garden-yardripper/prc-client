94e16729e3d8fb96bdd6475027cb03e8b4fcd345
gardenyardripper-ai <gardenyardripper@gmail.com>
2026-06-12 13:30:17 -0400

    Allow more location data types, add function docstring

--- src/prc/utils.py
+++ src/prc/utils.py
@@ -1,14 +1,25 @@
-from prc.v2.models import Location, Player, EmergencyCall
+from prc.v2.models import Location, MinimalLocation, Player, EmergencyCall
 import math
 
 def get_distance_between_locations(
-    location1: Location | Player | EmergencyCall,
-    location2: Location | Player | EmergencyCall
+    location1: Location | MinimalLocation | Player | EmergencyCall | tuple[int | float, int | float],
+    location2: Location | MinimalLocation | Player | EmergencyCall | tuple[int | float, int | float]
 ) -> float:
+    """Get the Euclidean distance between two locations as a float.
+    
+    Parameters
+    ----------
+    location1: `Location` | `Player` | `EmergencyCall`
+        The first location.
+    location2: `Location` | `Player` | `EmergencyCall`
+        The second location.
+    """
     if isinstance(location1, Player):
         position1 = location1.location.position
     elif isinstance(location1, EmergencyCall):
         position1 = location1.position.position
+    elif isinstance(location1, tuple):
+        position1 = location1
     else:
         position1 = location1.position
         
@@ -16,6 +27,8 @@ def get_distance_between_locations(
         position2 = location2.location.position
     elif isinstance(location2, EmergencyCall):
         position2 = location2.position.position
+    elif isinstance(location2, tuple):
+        position2 = location2
     else:
         position2 = location2.position
     
