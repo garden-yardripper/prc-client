a1e0bd37e56e66228a48dbbd83a8d00d35a2dce0
gardenyardripper-ai <gardenyardripper@gmail.com>
2026-06-12 14:54:33 -0400

    Add RateLimitConfig to exports in __init__.py

--- src/prc/__init__.py
+++ src/prc/__init__.py
@@ -1,6 +1,6 @@
 """`prc-client`: A flexible, feature-rich Python API client for the ER:LC private server API."""
 
 from . import v1, v2, utils, exceptions
-from .base_client import _BaseApiClient
+from .base_client import _BaseApiClient, RateLimitConfig
 
-__all__ = ["v1", "v2", "utils", "exceptions", "_BaseApiClient"]
+__all__ = ["v1", "v2", "utils", "exceptions", "_BaseApiClient", "RateLimitConfig"]
