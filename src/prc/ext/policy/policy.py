ba320f0c96a85df1cc9c23380a1696a638a2b8c6
gardenyardripper-ai <gardenyardripper@gmail.com>
2026-06-12 13:51:39 -0400

    Add init method to CommandPolicy

--- src/prc/ext/policy/policy.py
+++ src/prc/ext/policy/policy.py
@@ -1,2 +1,8 @@
+from prc.v2.command import normalize_command
+
 class CommandPolicy:
-    pass
+    def __init__(self, allowed: set[str], *, max_length: int = 120) -> None:
+        self.allowed = {normalize_command(cmd) for cmd in allowed}
+        self.max_length = max_length
+    
+    
