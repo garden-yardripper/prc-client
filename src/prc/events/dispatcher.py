ace135b29b8472bb83c1652eb56a29fc6301570a
garden-yardripper <gardenyardripper@gmail.com>
2026-06-14 22:27:59 -0400

    Implement Dispatcher class with methods to add event handlers and commands

---
+++ src/prc/events/dispatcher.py
@@ -0,0 +1,19 @@
+from typing import Callable, Literal
+
+ANY_COMMAND = object()
+
+class Dispatcher:
+    def __init__(self) -> None:
+        self.handlers: dict[str, list[Callable]] = {}
+        self.commands: dict[str | object, list[Callable]]
+        
+    def add_function(self,
+        func: Callable,
+        event_type: Literal["EmergencyCallStarted", "WebhookProbe"] | str
+    ):
+        self.handlers.setdefault(event_type, []).append(func)
+        
+    def add_command(self, func: Callable, name: str | None):
+        if name:
+            self.commands.setdefault(name, []).append(func)
+        self.commands.setdefault(ANY_COMMAND, []).append(func)
