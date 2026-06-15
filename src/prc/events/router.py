0fa47d64ecc707e4dc9600f6cea5fdb85c9fdeec
gardenyardripper-ai <gardenyardripper@gmail.com>
2026-06-15 13:49:22 -0400

    Remove the EventWebhook class
    Was using it for copy and paste.

--- src/prc/events/router.py
+++ src/prc/events/router.py
@@ -14,25 +14,6 @@ from .models import EventBatch
 
 ANY_COMMAND = object()
 
-class EventWebhook:
-    def __init__(self) -> None:
-        _raw_public_key = "MCowBQYDK2VwAyEAjSICb9pp0kHizGQtdG8ySWsDChfGqi+gyFCttigBNOA="
-        _public_key_bytes = base64.b64decode(_raw_public_key)
-        self.public_key = serialization.load_der_public_key(_public_key_bytes)
-    
-    def _verify_signature(self, raw_body: bytes, sighex: str, timestamp: str) -> bool:
-        if not isinstance(self.public_key, Ed25519PublicKey):
-            return False
-        
-        message = timestamp.encode() + raw_body
-        sighex_bytes = binascii.unhexlify(sighex)
-        
-        try:
-            self.public_key.verify(sighex_bytes, message)
-            return True
-        except InvalidSignature:
-            return False
-
 class Router:
     def __init__(self) -> None:
         self._handlers: dict[str, list[Callable]] = {}
