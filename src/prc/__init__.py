81b5d73a939fad607dfb9eb263727afad8591ae8
garden-yardripper <gardenyardripper@gmail.com>
2026-06-17 14:23:08 -0400

    Remove RateLimitConfig import from package __init__

--- src/prc/__init__.py
+++ src/prc/__init__.py
@@ -1,13 +1,11 @@
 """`prc-client`: A flexible, feature-rich Python API client for the ER:LC private server API."""
 
 from . import v1, v2, utils, exceptions
-from .base_client import RateLimitConfig
 from .users import FullUser, UsernameUser, IdUser
 from .command import cmd
 
 __all__ = [
-    "v1", "v2", "utils",
-    "exceptions", "RateLimitConfig",
+    "v1", "v2", "utils", "exceptions",
     "FullUser", "UsernameUser", "IdUser", "cmd"
 ]
 
