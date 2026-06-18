637ae1c3a54cc45acfd41be422479983c43d0570
garden-yardripper <gardenyardripper@gmail.com>
2026-06-17 22:34:07 -0400

    Rename class to UserCache and add resolve method

--- src/prc/user_cache.py
+++ src/prc/user_cache.py
@@ -1,9 +1,9 @@
 from .v2.models import Server, BundledServer
-from .users import FullUser
+from .users import FullUser, IdUser, StrictUserType, UsernameUser
 from .exceptions import DataNotRequestedError
 from itertools import chain
 
-class _UserCache:
+class UserCache:
     def __init__(self):
         self._by_id: dict[int, FullUser] = {}
         self._by_username: dict[str, FullUser] = {}
@@ -56,3 +56,39 @@ class _UserCache:
                self._by_username[log.moderator.name] = log.moderator
         except DataNotRequestedError:
             pass
+        
+    def resolve(self, user: StrictUserType) -> FullUser | None:
+        """Resolves a user reference to a `FullUser` object,
+        or returns `None` if the user is not found in the registry.
+        
+        Arguments
+        ---------
+        user: `StrictUserType`
+            A user reference to search for in the registry.
+            
+        Returns
+        -------
+        `FullUser` | `None`
+            The resolved `FullUser` object, or `None` if not found.
+        """
+        
+        if isinstance(user, FullUser):
+            return user
+        
+        id_search = None
+        name_search = None
+        if isinstance(user, UsernameUser):
+            name_search = user.name
+        elif isinstance(user, IdUser):
+            id_search = user.id
+        elif isinstance(user, str):
+            name_search = user
+        else:
+            id_search = user
+            
+        if id_search:
+            return self._by_id.get(id_search)
+        if name_search:
+            return self._by_username.get(name_search)
+        
+        return None
