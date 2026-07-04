46028fb2dcf5e6a840d892f6d7c38f6ade5fbcb2
garden-yardripper <gardenyardripper@gmail.com>
2026-07-04 12:06:58 -0400

    Fix BackgroundTask import in starlette integration

--- src/prc/events/integrations/starlette.py
+++ src/prc/events/integrations/starlette.py
@@ -12,13 +12,18 @@ class _StarletteIntegration:
     async def handle_starlette_request(
         self, request: "Request"
     ) -> tuple[Literal[200, 400], "BackgroundTask | None"]:
+        try:
+            from starlette.background import BackgroundTask
+        except ImportError:
+            raise RuntimeError("Starlette integration for PRC events requires the `starlette` library.")
+        
         raw_body = await request.body()
         headers = dict(request.headers)
-
+        
         status, task = await self.router.prepare_request(raw_body, headers)
         if status == 200 and task:
             background_task = BackgroundTask(task)
         else:
             background_task = None
-
+        
         return status, background_task
