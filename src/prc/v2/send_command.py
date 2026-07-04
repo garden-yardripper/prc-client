dc97e9f4ce50d887ffb6aaa4c43e89e169e6181d
garden-yardripper <gardenyardripper@gmail.com>
2026-07-03 22:15:48 -0400

    Add server_key parameter to internal _SendCommand methods

--- src/prc/v2/send_command.py
+++ src/prc/v2/send_command.py
@@ -9,22 +9,36 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 class _SendCommand(_BaseApiClient):
-    async def _send_command_async(self, command: "CommandLike") -> Response:
+    async def _send_command_async(self, command: "CommandLike", *, server_key: str | None = None) -> Response:
         from ..command import normalize_command
         cmd = command if isinstance(command, str) else command.text
+        
         logger.info("Sending command: %s", cmd)
+        if server_key is not None:
+            return await self._send_async_request(
+                endpoint=_Endpoint.v2_command,
+                json={"command": normalize_command(cmd)},
+                headers={"server-key": server_key, "Authorization": self.global_key}
+            )
+        else:
+            return await self._send_async_request(
+                endpoint=_Endpoint.v2_command,
+                json={"command": normalize_command(cmd)}
+            )
         
-        return await self._send_async_request(
-            endpoint=_Endpoint.v2_command,
-            json={"command": normalize_command(cmd)}
-        )
-        
-    def _send_command_sync(self, command: "CommandLike") -> Response:
+    def _send_command_sync(self, command: "CommandLike", *, server_key: str | None = None) -> Response:
         from ..command import normalize_command
         cmd = command if isinstance(command, str) else command.text
+
         logger.info("Sending command: %s", cmd)
-        
-        return self._send_sync_request(
-            endpoint=_Endpoint.v2_command,
-            json={"command": normalize_command(cmd)}
-        )
+        if server_key is not None:
+            return self._send_sync_request(
+                endpoint=_Endpoint.v2_command,
+                json={"command": normalize_command(cmd)},
+                headers={"server-key": server_key, "Authorization": self.global_key}
+            )
+        else:
+            return self._send_sync_request(
+                endpoint=_Endpoint.v2_command,
+                json={"command": normalize_command(cmd)}
+            )
