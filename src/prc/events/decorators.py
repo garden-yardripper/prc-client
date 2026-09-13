01c5f8874e4bb40289c6fc4f01c5dc600f7a87ce
garden-yardripper <gardenyardripper@gmail.com>
2026-09-13 13:23:59 -0400

    Implement better typing for event decorators

--- src/prc/events/decorators.py
+++ src/prc/events/decorators.py
@@ -1,17 +1,17 @@
-from typing import TYPE_CHECKING, Any, Callable
+from typing import Any, Callable
 
 from ..v2.client import AsyncClient, Client
 from .models import Context
-if TYPE_CHECKING:
-    from .router import Router
+from .router import Router
 
 type _EventHandler[T: (Client, AsyncClient)] = Callable[[Context[T]], Any]
+type _EventDecorator[T: (Client, AsyncClient)] = Callable[[_EventHandler[T]], _EventHandler[T]]
 
-class _On:
-    def __init__(self, router: "Router") -> None:
+class _On[T: (Client, AsyncClient)]:
+    def __init__(self, router: Router) -> None:
         self.router = router
         
-    def command[T: (Client, AsyncClient)](self, command: str, *commands: str):
+    def command(self, command: str, *commands: str) -> _EventDecorator[T]:
         """Register a function to be called when a specific command is run in-game."""
         def wrapper(func: _EventHandler[T]) -> _EventHandler[T]:
             self.router._add_command(func, command)
@@ -20,14 +20,14 @@ class _On:
             return func
         return wrapper
     
-    def any_custom_command[T: (Client, AsyncClient)](self):
+    def any_custom_command(self) -> _EventDecorator[T]:
         """Register a function to be called when any custom command is run in-game."""
         def wrapper(func: _EventHandler[T]) -> _EventHandler[T]:
             self.router._add_command(func, None)
             return func
         return wrapper
     
-    def custom_event[T: (Client, AsyncClient)](self, event_name: str, *event_names: str):
+    def custom_event(self, event_name: str, *event_names: str) -> _EventDecorator[T]:
         """Register a function to be called when a custom event with the specified name is received."""
         def wrapper(func: _EventHandler[T]) -> _EventHandler[T]:
             self.router._add_function(func, event_name)
@@ -36,14 +36,14 @@ class _On:
             return func
         return wrapper
     
-    def emergency_start[T: (Client, AsyncClient)](self):
+    def emergency_start(self) -> _EventDecorator[T]:
         """Register a function to be called when an emergency call is made in-game."""
         def wrapper(func: _EventHandler[T]) -> _EventHandler[T]:
             self.router._add_function(func, "EmergencyCallStarted")
             return func
         return wrapper
     
-    def probe[T: (Client, AsyncClient)](self):
+    def probe(self) -> _EventDecorator[T]:
         """Register a function to be called when a webhook ping is received from the PRC API.
         
         These are occasionally sent by the API to test the validity of the webhook URL and its signature verification.
@@ -53,7 +53,7 @@ class _On:
             return func
         return wrapper
     
-    def any_event[T: (Client, AsyncClient)](self):
+    def any_event(self) -> _EventDecorator[T]:
         """Register a function to be called when any non-command event is received."""
         def wrapper(func: _EventHandler[T]) -> _EventHandler[T]:
             self.router._add_function(func, None)
